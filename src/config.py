897f71a5dc94f3737f99a408b170420a436e336b
Felipe Kanashiro <felipekanashiro@live.com>
2022-06-25 15:36:40 +0000

    Added configuration file for Supercell API token and clan tag

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+import configparser
+from pathlib import Path
+
+def getConfigValue(_section, _key):
+    config = configparser.ConfigParser()
+    dir = os.path.dirname(__file__)
+    config.read(str(Path(dir).parent) + '/config.ini')
+    return config[_section][_key]    
+
+def getBearerToken():    
+    return 'Bearer ' + getConfigValue('coc-api', 'BearerToken')
+
+def getClanTag():    
+    return getConfigValue('coc-api', 'ClanTag').replace('#', '%23')
