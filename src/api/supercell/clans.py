60d3f2eda997892a5c5ae5bba8972b63d11b7326
Felipe Kanashiro <kanasec@proton.me>
2022-07-21 00:09:34 +0000

    Added exceptions

--- src/api/supercell/clans.py
+++ src/api/supercell/clans.py
@@ -1,15 +1,40 @@
+from inspect import trace
 import requests
 import config
+import json
+import traceback
+from models import currentwar_inwar, currentwar_preparation
+
+def getCW_obj(cwjson):
+  try:
+    cw = json.loads(cwjson)
+    
+    if cw['state'] == 'preparation':
+      return currentwar_preparation.Root.from_dict(cw)
+    elif cw['state'] == 'inWar':
+      return currentwar_inwar.Root.from_dict(cw)
+    else:
+      raise Exception("Cannot deserialize JSON into object: Invalid state")
+  except Exception as e:
+    raise Exception("Cannot deserialize JSON into object: " + str(e))
+
 
 def get_currentwar():
-    url = "https://api.clashofclans.com/v1/clans/" + config.getClanTag() + "/currentwar"
-
-    payload = ""
-    headers = {
-        'Authorization': config.getBearerToken()
-    }
-
-    return requests.request("GET", url, headers=headers, data=payload)
+    try:
+        url = "https://api.clashofclans.com/v1/clans/" + config.getClanTag() + "/currentwar"
+    
+        payload = ""
+        headers = {
+            'Authorization': config.getBearerToken()
+        }
+    
+        rq = requests.request("GET", url, headers=headers, data=payload)
+        if(rq.status_code == 200):
+            return getCW_obj(rq.text)
+        else:
+            raise Exception("Request error\nURL=" + url + "\n" + rq.status_code + " - " + rq.text)
+    except Exception as e:
+        raise Exception("Request error\nURL=" + url + "\n" + str(e))
 
 def get_warleague(_wartag):
     url = "https://api.clashofclans.com/v1/clanwarleagues/wars/" + _wartag
--- src/run.py
+++ src/run.py
@@ -9,40 +9,28 @@ from models import currentwar_inwar, currentwar_preparation
 def localtest():
   return False
 
-def getCW_Obj(_cwjson):
-  cw = json.loads(_cwjson)
-  
-  if cw['state'] == 'preparation':
-    return currentwar_preparation.Root.from_dict(cw)
-  elif cw['state'] == 'inWar':
-    return currentwar_inwar.Root.from_dict(cw)
-
 def run():
-  if localtest():
-    file = open(str(pathlib.Path(__file__).parent.resolve()) + '/examples/currentwar_preparation.json', 'r')
-    _cw = getCW_Obj(file.read())
-  else:
-    currentWar = clans.get_currentwar()
-  
-    if currentWar.status_code == 200:
-      _cw = getCW_Obj(currentWar.text)
+  try:
+    if localtest():
+      file = open(str(pathlib.Path(__file__).parent.resolve()) + '/examples/currentwar_preparation.json', 'r')
+      cw = clans.getCW_obj(file.read())
     else:
-      print(currentWar.text + str(currentWar.status_code))
-      exit()
+      cw = clans.get_currentwar()
 
-  if isinstance(_cw, currentwar_preparation.Root):
-    _starttime = datetime.strptime(_cw.startTime, "%Y%m%dT%H%M%S.%fZ")
-    #_starttime = _starttime.astimezone(pytz.timezone('America/Sao_Paulo'))    
+    if isinstance(cw, currentwar_preparation.Root):
+      start_time = datetime.strptime(cw.startTime, "%Y%m%dT%H%M%S.%fZ")
+      #_starttime = _starttime.astimezone(pytz.timezone('America/Sao_Paulo'))    
 
-    _remainingTime = (_starttime - timedelta(hours = 3)) - datetime.now()
-  elif isinstance(_cw, currentwar_inwar.Root):
-    _endtime = datetime.strptime(_cw.endTime, "%Y%m%dT%H%M%S.%fZ")
-    #_endtime = _starttime.astimezone(pytz.timezone('America/Sao_Paulo'))    
+      remaining_time = (start_time - timedelta(hours = 3)) - datetime.now()
+    elif isinstance(cw, currentwar_inwar.Root):
+      endtime = datetime.strptime(cw.endTime, "%Y%m%dT%H%M%S.%fZ")
+      #_endtime = _starttime.astimezone(pytz.timezone('America/Sao_Paulo'))
 
-    _remainingTime = (_endtime - timedelta(hours = 3)) - datetime.now()
-  else:
-    print(currentWar.text + str(currentWar.status_code))
+      remaining_time = (endtime - timedelta(hours = 3)) - datetime.now()    
 
-  print(_remainingTime)
+    print(remaining_time)
+  except Exception as e:
+    print(str(e))
+    exit()
 
 run()
