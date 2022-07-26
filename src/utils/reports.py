5c08939d524053bd37d9dfbd848311303e9ac3fa
Felipe Kanashiro <kanasec@proton.me>
2022-07-26 00:27:04 +0000

    fix: remaining minutes reports.py

--- src/utils/reports.py
+++ src/utils/reports.py
@@ -2,10 +2,13 @@ from datetime import datetime, timedelta
 from math import floor
 
 def remaining_time_str(rt):
-    minutes = floor(rt.total_seconds() / 60)
-    hours = (floor(rt.total_seconds() / 60 / 60))
+    minutes = floor((rt.total_seconds() // 60) % 60)
+    hours = floor(rt.total_seconds() / 60 / 60)
 
-    if (hours == 0):
+    if hours == 0:
         return str(minutes) + " minutos"
     else:
-        return str(hours) + " horas e " + str(minutes) + " minutos"
+        if hours == 1:
+            return str(hours) + " hora e " + str(minutes) + " minutos"
+        else:
+            return str(hours) + " horas e " + str(minutes) + " minutos"
