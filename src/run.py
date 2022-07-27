3c5c279d7263494c04cf25d7fb0dd583c7dfbf8e
Felipe Kanashiro <kanasec@proton.me>
2022-07-27 01:50:06 +0000

    add inwar remaining attacks report

--- src/run.py
+++ src/run.py
@@ -21,16 +21,17 @@ def run():
     if isinstance(cw, currentwar_preparation.Root):    
       start_time = utils.reports.remaining_local_time(cw.startTime)
 
-      with open(str(pathlib.Path(__file__).parent.resolve()) + '/reports/war_preparation.txt', mode = 'r') as f:
-        report = f.read()
+      with open(str(pathlib.Path(__file__).parent.resolve()) + '/reports/war_preparation.txt', mode = 'r') as prep_file:
+        prep_report = prep_file.read()
 
-      print(report.format(clan = cw.clan.name, remaining_hours = utils.reports.remaining_time_str(start_time)))
+      print(prep_report.format(clan = cw.clan.name, remaining_hours = utils.reports.remaining_time_str(start_time)))
     elif isinstance(cw, currentwar_inwar.Root):
       end_time = utils.reports.remaining_local_time(cw.endTime)
 
-      #get members with remaining attacks to do
-      attacks = [m for m in cw.clan.members if len(m.attacks) < cw.attacksPerMember]
-      print(attacks)
+      with open(str(pathlib.Path(__file__).parent.resolve()) + '/reports/war_inwar.txt', mode = 'r') as inwar_file:
+        inwar_report = inwar_file.read()
+
+      print(inwar_report.format(clan = cw.clan.name, remaining_hours = utils.reports.remaining_time_str(end_time), remaining_attacks = utils.reports.remaining_attacks_bymember_str(cw.clan.members, cw.attacksPerMember)))
   except Exception as e:
     print(str(e))
     exit()
--- src/utils/reports.py
+++ src/utils/reports.py
@@ -17,3 +17,14 @@ def remaining_time_str(rt):
 def remaining_local_time(t):
     rt = datetime.strptime(t, "%Y%m%dT%H%M%S.%fZ")
     return (rt - timedelta(hours = 3)) - datetime.now()
+
+def remaining_attacks_bymember_str(members, attacksPerMember):
+    #get members with remaining attacks to do
+    members = [m for m in members if len(m.attacks) < attacksPerMember]
+
+    attacks_str = ""
+
+    for m in members:
+        attacks_str += m.name + " - " + str(attacksPerMember - len(m.attacks)) + " ataques restantes\n"
+
+    return attacks_str[:-1]
